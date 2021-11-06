ab59d25952ab3a980a79b52399aaf889bc5aa8f7
Steve <34773104+SoloSteve@users.noreply.github.com>
2021-11-06 23:00:25 +0200

    Dev (#2)
    * Added .gitignore
    
    * Added previous backend code
    
    * initialized frontend with framework7 cli
    
    * added navigation skeleton
    
    * Added group page with group list. Contains placeholder values.
    
    * Added placeholder stats to contact list
    
    * Extracted statistics from contact list. Added comments to app.js
    
    * Added leaflet map.
    
    * Extracted user marker and added odometer.
    
    * Setup create & get room endpoints.
    
    * added websocket implementation. (Save commit)
    
    * added websocket communication between front and back end.
    
    * Working data upstream
    
    * updated static assets
    
    * added settings
    
    * added group functionality
    
    * added accuracy and heading to marker
    
    * added distance and fixed accuracy
    
    * working alpha version
    
    * fixed display issues and fixed server environment bug
    
    * added automatic room deletion
    
    * fixed bug where icon would stay standing man
    
    * quality of life improvements
    
    * added waypoints and minor fixes
    
    * fixed bug where auto center wouldn't work
    
    * fixed some bugs and other stuff I don't remember
    
    * added extra permissions to fix IOS 13 incompatibility
    
    * fixed odometer, zoom, and waypoint issues
    
    * fixed arrow zoom
    
    * added readme content
    
    * upgraded packages
    
    * upgraded packages

--- backend/src/loaders/websocket.ts
+++ backend/src/loaders/websocket.ts
@@ -5,13 +5,13 @@ Websocket Server Loader
 Bootstrap the websocket server instance.
  */
 
-import * as io from "socket.io";
-import {Server} from "http";
+import {Server as SocketServer} from "socket.io";
+import {Server as HttpServer} from "http";
 import {defaultWebsocketOptions} from "../config/websocket";
 import env from "../config/environment";
 
-export default (httpServer: Server): io.Server => {
-  return io(httpServer, {
+export default (httpServer: HttpServer): SocketServer => {
+  return new SocketServer(httpServer, {
     path: `/${env.INSTANCE_ID}`,
     pingInterval: defaultWebsocketOptions.pingInterval,
     pingTimeout: defaultWebsocketOptions.pingTimeout,
